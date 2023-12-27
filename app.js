09d346ce7e52122744e776eee16a7d06b46b5c0a
yahm0003 <110844494+yahm0003@users.noreply.github.com>
2023-12-27 23:10:44 +1100

    added unicorn feature
    clicking unicorn will make player lose point

--- app.js
+++ app.js
@@ -12,16 +12,30 @@ let timerId = null
 function randomSquare() {
     squares.forEach(square => {
         square.classList.remove('mole')
+        square.classList.remove('unicorn')
     })
     let randomSquare = squares[Math.floor(Math.random()*9)]
-    randomSquare.classList.add('mole')
-    hitPosition = randomSquare.id
+    let chance = Math.random()
+    if (chance<0.5) {
+        randomSquare.classList.add('mole')
+        hitPosition = randomSquare.id
+    }
+    else {
+        randomSquare.classList.add('unicorn')
+        hitPosition = randomSquare.id
+    }
 }
 
 squares.forEach(square => {
     square.addEventListener('click', () => {
-        if (square.id == hitPosition) {
+        if ((square.id == hitPosition) && (square.classList.contains('mole'))) {
             result++
+            console.log(result)
+            score.textContent = result
+            hitPosition = null
+        }
+        else if ((square.id == hitPosition) && (square.classList.contains('unicorn'))) {
+            result--
             console.log(result)
             score.textContent = result
             hitPosition = null
@@ -42,6 +56,10 @@ function countDown() {
         clearInterval(countDownTimerId)
         clearInterval(timerId)
         timeRemaining.textContent = 0
+        if (result <= -1) {
+            result = 0
+            score.textContent = result
+        }
         alert("Time's up! Your final score: " + result)
     }
 }
